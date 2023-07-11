45dc2e78dfa74c547b52ce3508c658a8ff2c3737
Coriana Williams <corianacory@gmail.com>
2023-07-11 14:48:08 -0400

    made updates to store index

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { legacy_createStore as createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
-import reducer from './reducer';
+import categoryReducer from './categories';
+import productsReducer from './products';
+import cartReducer from './cart';
 
 const reducers = combineReducers({
-  store: reducer,
+  categories: categoryReducer,
+  products: productsReducer,
+  cart: cartReducer
 });
 
 const store = () => createStore(reducers, composeWithDevTools());
